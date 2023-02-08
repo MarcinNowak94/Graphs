8299480c63f2c3fa017588dbb7f81df92834cec1
Marcin Nowak <SourVenom1994@gmail.com>
2023-02-08 23:30:14 +0100

    Changed to weighted graph, Added Visualization

--- Graphs.py
+++ Graphs.py
@@ -6,22 +6,23 @@
 #https://github.com/networkx/networkx
 #Environment preparation: pip install networkx
 
+import matplotlib.pyplot as plt
 import networkx as nx   #advised naming convention
+
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
+def DisplayGraph(graph, pos):
+    
+    #pos = nx.spring_layout(graph, seed=3068)  # Seed layout for reproducibility
+    nx.draw(graph, pos=pos, with_labels=True)
+    #plt.title(label="Graf")
+    plt.show()
 
 
 #LAB 4.2 Example
-example = { "START" : {"A", "C"},
-      "A" : {"B", "D"},
-      "B" : {"META"},
-      "C" : {"D"},
-      "D" : {"B", "META"},
-      "META" : {}
-    }
-example2=[
-    ("START", "A", 5), 
+example=[
+    ("START", "A", 5),
     ("START", "C",6),
     ("A","B",1),
     ("A","D",2),
@@ -30,15 +31,50 @@ example2=[
     ("D","B",7),
     ("D","META",3)
 ]
+pos={
+    "START": [-2, 0],
+    "A":    [-1,1],
+    "B":    [1,1],
+    "C":    [-1,-1],
+    "D":    [1,-1],
+    "META":    [2,0]
+}
+G = nx.DiGraph(name="Graf")
 
-FG = nx.Graph()
+G.add_weighted_edges_from(example)
 
-FG.add_weighted_edges_from([(1, 2, 0.125), (1, 3, 0.75), (2, 4, 1.2), (3, 4, 0.375)])
+pathlengths = []
 
-for n, nbrs in FG.adj.items():
+print("source vertex {target:length, }")
+for v in G.nodes():
+    spl = dict(nx.single_source_shortest_path_length(G, v))
+    print(f"{v} {spl} ")
+    for p in spl:
+        pathlengths.append(spl[p])
 
-   for nbr, eattr in nbrs.items():
+print()
+print(f"average shortest path length {sum(pathlengths) / len(pathlengths)}")
 
-       wt = eattr['weight']
+# histogram of path lengths
+dist = {}
+for p in pathlengths:
+    if p in dist:
+        dist[p] += 1
+    else:
+        dist[p] = 1
 
-       if wt < 0.5: print(f"({n}, {nbr}, {wt:.3})")
+print()
+print("length #paths")
+verts = dist.keys()
+for d in sorted(verts):
+    print(f"{d} {dist[d]}")
+
+#print(nx.is_connected(G))
+#print(f"radius: {nx.radius(G)}")
+#print(f"diameter: {nx.diameter(G)}")
+#print(f"eccentricity: {nx.eccentricity(G)}")
+#print(f"center: {nx.center(G)}")
+#print(f"periphery: {nx.periphery(G)}")
+#print(f"density: {nx.density(G)}")
+
+DisplayGraph(G, pos)
