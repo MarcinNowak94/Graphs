d7def07dba1ecf80afffb8c278605e1e5f305dd5
Marcin Nowak <SourVenom1994@gmail.com>
2023-02-08 20:08:56 +0100

    Changed to networkx package

--- Graphs.py
+++ Graphs.py
@@ -1,170 +1,12 @@
+
+#Useful resources: 
 #https://www.khanacademy.org/computing/computer-science/algorithms/graph-representation/a/representing-graphs
 #https://stackoverflow.com/questions/40040304/creating-weighted-directed-graph-in-python-based-on-user-input
 
-#-------------------------------------------------------------------------------
-#Implementation: https://python-course.eu/applications-python/graphs-python.php
-#-------------------------------------------------------------------------------
+#https://github.com/networkx/networkx
+#Environment preparation: pip install networkx
 
-""" A Python Class
-A simple Python graph class, demonstrating the essential 
-facts and functionalities of graphs.
-"""
-
-
-class Graph(object):
-
-    def __init__(self, graph_dict=None):
-        """ initializes a graph object 
-            If no dictionary or None is given, 
-            an empty dictionary will be used
-        """
-        if graph_dict == None:
-            graph_dict = {}
-        self._graph_dict = graph_dict
-
-    def edges(self, vertice):
-        """ returns a list of all the edges of a vertice"""
-        return self._graph_dict[vertice]
-        
-    def all_vertices(self):
-        """ returns the vertices of a graph as a set """
-        return set(self._graph_dict.keys())
-
-    def all_edges(self):
-        """ returns the edges of a graph """
-        return self.__generate_edges()
-
-    def add_vertex(self, vertex):
-        """ If the vertex "vertex" is not in 
-            self._graph_dict, a key "vertex" with an empty
-            list as a value is added to the dictionary. 
-            Otherwise nothing has to be done. 
-        """
-        if vertex not in self._graph_dict:
-            self._graph_dict[vertex] = []
-
-    def add_edge(self, edge):
-        """ assumes that edge is of type set, tuple or list; 
-            between two vertices can be multiple edges! 
-        """
-        edge = set(edge)
-        vertex1, vertex2 = tuple(edge)
-        for x, y in [(vertex1, vertex2), (vertex2, vertex1)]:
-            if x in self._graph_dict:
-                self._graph_dict[x].append(y)
-            else:
-                self._graph_dict[x] = [y]
-
-    def __generate_edges(self):
-        """ A static method generating the edges of the 
-            graph "graph". Edges are represented as sets 
-            with one (a loop back to the vertex) or two 
-            vertices 
-        """
-        edges = []
-        for vertex in self._graph_dict:
-            for neighbour in self._graph_dict[vertex]:
-                if {neighbour, vertex} not in edges:
-                    edges.append({vertex, neighbour})
-        return edges
-    
-    def __iter__(self):
-        self._iter_obj = iter(self._graph_dict)
-        return self._iter_obj
-    
-    def __next__(self):
-        """ allows us to iterate over the vertices """
-        return next(self._iter_obj)
-
-    def __str__(self):
-        res = "vertices: "
-        for k in self._graph_dict:
-            res += str(k) + " "
-        res += "\nedges: "
-        for edge in self.__generate_edges():
-            res += str(edge) + " "
-        return res
-
-    def find_path(self, start_vertex, end_vertex, path=None):
-        """ find a path from start_vertex to end_vertex 
-            in graph """
-        if path == None:
-            path = []
-        graph = self._graph_dict
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            return path
-        if start_vertex not in graph:
-            return None
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_path = self.find_path(vertex, 
-                                               end_vertex, 
-                                               path)
-                if extended_path: 
-                    return extended_path
-        return None
-    
-    
-    def find_all_paths(self, start_vertex, end_vertex, path=[]):
-        """ find all paths from start_vertex to 
-            end_vertex in graph """
-        graph = self._graph_dict 
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            return [path]
-        if start_vertex not in graph:
-            return []
-        paths = []
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_paths = self.find_all_paths(vertex, 
-                                                     end_vertex, 
-                                                     path)
-                for p in extended_paths: 
-                    paths.append(p)
-        return paths
-
-
-class Graph2(Graph):
-    
-    def vertex_degree(self, vertex):
-        """ The degree of a vertex is the number of edges connecting
-        it, i.e. the number of adjacent vertices. Loops are counted 
-        double, i.e. every occurence of vertex in the list 
-        of adjacent vertices. """ 
-        degree =  len(self._graph_dict[vertex]) 
-        if vertex in self._graph_dict[vertex]:
-            degree += 1
-        return degree
-
-    def find_isolated_vertices(self):
-        """ returns a list of isolated vertices. """
-        graph = self._graph_dict
-        isolated = []
-        for vertex in graph:
-            print(isolated, vertex)
-            if not graph[vertex]:
-                isolated += [vertex]
-        return isolated
-        
-    def Delta(self):
-        """ the maximum degree of the vertices """
-        max = 0
-        for vertex in self._graph_dict:
-            vertex_degree = self.vertex_degree(vertex)
-            if vertex_degree > max:
-                max = vertex_degree
-        return max
-    
-    def degree_sequence(self):
-        """ calculates the degree sequence """
-        seq = []
-        for vertex in self._graph_dict:
-            seq.append(self.vertex_degree(vertex))
-        seq.sort(reverse=True)
-        return tuple(seq)
-
+import networkx as nx   #advised naming convention
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
@@ -178,11 +20,25 @@ example = { "START" : {"A", "C"},
       "D" : {"B", "META"},
       "META" : {}
     }
+example2=[
+    ("START", "A", 5), 
+    ("START", "C",6),
+    ("A","B",1),
+    ("A","D",2),
+    ("C","D",8),
+    ("B","META",6),
+    ("D","B",7),
+    ("D","META",3)
+]
 
+FG = nx.Graph()
 
-graph = Graph2(example)
+FG.add_weighted_edges_from([(1, 2, 0.125), (1, 3, 0.75), (2, 4, 1.2), (3, 4, 0.375)])
 
-print("All edges: ")
-print(graph.all_edges())
+for n, nbrs in FG.adj.items():
 
-print(graph.degree_sequence())
+   for nbr, eattr in nbrs.items():
+
+       wt = eattr['weight']
+
+       if wt < 0.5: print(f"({n}, {nbr}, {wt:.3})")
