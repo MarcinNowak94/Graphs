5df783ae7f3b0cb0a33db5fa20f696c11c6f947b
Marcin Nowak <SourVenom1994@gmail.com>
2023-02-09 00:13:56 +0100

    Added edge emphasis, added DisplayGraphData

--- Graphs.py
+++ Graphs.py
@@ -8,23 +8,77 @@
 
 import matplotlib.pyplot as plt
 import networkx as nx   #advised naming convention
+import time
 
 Config={
     "NodeSize": 700,
-    "EdgeWidth": 3,
+    "EdgeWidth": 2,
+    "EmphasizedEdgeWidth": 6,
     "FontSize": 15,
     "FontFamily": "sans-serif"
 }
 
 #-------------------------------------------------------------------------------
+def CalculateCriticalPath(G):
+    emph={
+        "src":"",
+        "dst":""
+    }
+
+    for edge in G.edges:
+        emph["src"]=edge[0]
+        emph["dst"]=edge[1]
+        
+        DisplayGraph(G, pos, emph)
+    print("Not ready yet, please come again later\n")
 #-------------------------------------------------------------------------------
-def DisplayGraph(G, pos):
+def DisplayGraphData(G, issimple):
+    pathlengths = []
+
+    print("source vertex {target:length, }")
+    for v in G.nodes():
+        spl = dict(nx.single_source_shortest_path_length(G, v))
+        print(f"{v} {spl} ")
+        for p in spl:
+            pathlengths.append(spl[p])
+
+    print()
+    print(f"average shortest path length {sum(pathlengths) / len(pathlengths)}")
+
+    # histogram of path lengths
+    dist = {}
+    for p in pathlengths:
+        if p in dist:
+            dist[p] += 1
+        else:
+            dist[p] = 1
+
+    print()
+    print("length #paths")
+    verts = dist.keys()
+    for d in sorted(verts):
+        print(f"{d} {dist[d]}")
+
+    if (issimple==True):
+        print(nx.is_connected(G))
+        print(f"radius: {nx.radius(G)}")
+        print(f"diameter: {nx.diameter(G)}")
+        print(f"eccentricity: {nx.eccentricity(G)}")
+        print(f"center: {nx.center(G)}")
+        print(f"periphery: {nx.periphery(G)}")
+        print(f"density: {nx.density(G)}")
+
+#TODO: nicely step over, currently 
+def DisplayGraph(G, pos, emph):
     # nodes
     nx.draw_networkx_nodes(G, pos, node_size=Config["NodeSize"])
 
     # edges
-    edges = [(u, v) for (u, v, d) in G.edges(data=True)]
+    edges = [(u, v) for (u, v, d) in G.edges(data=True) if not(u==emph["src"] and v==emph["dst"])]
+    enlarged = [(u, v) for (u, v, d) in G.edges(data=True) if (u==emph["src"] and v==emph["dst"]) ]
+    
     nx.draw_networkx_edges(G, pos, edgelist=edges, width=Config["EdgeWidth"])
+    nx.draw_networkx_edges(G, pos, edgelist=enlarged, width=Config["EmphasizedEdgeWidth"])
 
     # node labels
     nx.draw_networkx_labels(G, pos, font_size=Config["FontSize"], font_family=Config["FontFamily"])
@@ -38,7 +92,6 @@ def DisplayGraph(G, pos):
     plt.axis("off")
     plt.tight_layout()
     plt.show()
-
 
 #LAB 4.2 Example
 example=[
@@ -60,41 +113,16 @@ pos={
     "META":    [2,0]
 }
 G = nx.DiGraph(name="Graf")
-
+issimplegraph=False
 G.add_weighted_edges_from(example)
 
-pathlengths = []
+DisplayGraphData(G, issimplegraph)
 
-print("source vertex {target:length, }")
-for v in G.nodes():
-    spl = dict(nx.single_source_shortest_path_length(G, v))
-    print(f"{v} {spl} ")
-    for p in spl:
-        pathlengths.append(spl[p])
+emph={
+    "src":"",
+    "dst":""
+}
+DisplayGraph(G, pos, emph)
 
-print()
-print(f"average shortest path length {sum(pathlengths) / len(pathlengths)}")
 
-# histogram of path lengths
-dist = {}
-for p in pathlengths:
-    if p in dist:
-        dist[p] += 1
-    else:
-        dist[p] = 1
-
-print()
-print("length #paths")
-verts = dist.keys()
-for d in sorted(verts):
-    print(f"{d} {dist[d]}")
-
-#print(nx.is_connected(G))
-#print(f"radius: {nx.radius(G)}")
-#print(f"diameter: {nx.diameter(G)}")
-#print(f"eccentricity: {nx.eccentricity(G)}")
-#print(f"center: {nx.center(G)}")
-#print(f"periphery: {nx.periphery(G)}")
-#print(f"density: {nx.density(G)}")
-
-DisplayGraph(G, pos)
+CalculateCriticalPath(G)
